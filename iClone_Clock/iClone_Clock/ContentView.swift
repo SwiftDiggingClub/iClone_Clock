aeac89ca28d03b3cdea04661d95c6d09b0cd3c3d
hyunjun <hyunjuntyler@gmail.com>
2023-08-16 16:13:43 +0900

    Initial commit

---
+++ iClone_Clock/iClone_Clock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iClone_Clock
+//
+//  Created by hyunjun on 2023/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iClone_Clock/iClone_Clock/iClone_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  iClone_ClockApp.swift
+//  iClone_Clock
+//
+//  Created by hyunjun on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct iClone_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
